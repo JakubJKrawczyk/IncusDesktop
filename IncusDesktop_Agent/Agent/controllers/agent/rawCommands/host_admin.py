8cfefb70ec757bb1e0a0fb204cbb4ca39d2f1af5
JakubJKrawczyk <jakub.krawczyk@buddy.works>
2026-06-10 14:44:48 +0200

    Its time for scenarios

---
+++ IncusDesktop_Agent/Agent/controllers/agent/rawCommands/host_admin.py
@@ -0,0 +1,9 @@
+from Utilities import consts
+from Utilities.logger import Logger
+
+logger = Logger("[RAW.HOST_ADMIN]", consts.ConfigVariables.DEFAULT_LOGS_INCUS.value)
+
+class HostAdminController:
+
+    def dupa():
+        pass
